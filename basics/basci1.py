53831208667dfd63ba70840c928b0caa192e92be
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-18 10:36:01 +0530

    basics

---
+++ basics/basci1.py
@@ -0,0 +1,26 @@
+print("Hello",end=",")
+print("I am Python")
+
+# int ,float,bool,string
+# int age =
+
+age = 18 # int
+print(age)
+print("age is ",age)
+print("type of age is ",type(age))
+
+per = 78.90 # float
+print(per)
+print("type of per is ",type(per))
+
+isPassed = True # bool
+print(isPassed)
+print("type of isPassed is ",type(isPassed))
+
+name = "Amit"
+print(name)
+print("type of name is ",type(name))
+
+choice = 'y'
+print(choice)
+print("type of choice is ",type(choice))
